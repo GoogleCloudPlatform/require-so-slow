0dd74bff4f24bbce8521e19228605b7a971fb599
Justin Beckwith <justin.beckwith@gmail.com>
2018-04-09 14:33:36 -0700

    pr feedback

--- src/cli.ts
+++ src/cli.ts
@@ -3,28 +3,31 @@
 import {execSync} from 'child_process';
 import * as path from 'path';
 import * as shim from './shim';
+import * as npa from 'npm-package-arg';
 
-// Parameters can be provided in the forms of:
-// $ rss googleapis
-// $ rss googleapis@20
-// $ rss googleapis@28.1.0
-// $ rss googleapis output.trace
+
+const usage = `Parameters can be provided in the forms of:
+  $ rss googleapis
+  $ rss googleapis@20
+  $ rss googleapis@28.1.0
+  $ rss googleapis output.trace
+`;
 
 const args = process.argv.slice(2);
 if (args.length < 1) {
-  console.error(`USAGE: rss some-npm-module [output]`);
+  console.error(usage);
   process.exit();
 }
 
 const mod = args[0];
-const [modName, modVer] = mod.split('@');
-const outputPath = args.length > 1 ? args[1] : `${mod}.trace`;
+const parsed = npa(mod);
+const outputPath = args.length > 1 ? args[1] : `${parsed.escapedName}.trace`;
 const relativePath = path.relative(process.cwd(), outputPath);
 
 execSync(`npm install --no-save ${mod}`, {stdio: 'inherit'});
 
 shim.createShim();
-require(modName);
+require(parsed.name!);
 shim.write(outputPath);
 
 console.info(`✨ Trace data written to \`${relativePath}\` ✨`);
